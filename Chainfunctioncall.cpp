9e49c7e8c51ccc6c166fa2ea18465cf477355e24
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-05 19:23:36 +0700

    menambah chaining function di class buku

---
+++ Chainfunctioncall.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    // Fungsi untuk set judul yang mengembalikan referensi ke objek itu sendiri (*this)
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this; // Inilah kunci dari chain function
+    }
+     string getJudul() {
+        return this->judul;
+    }
+};
